70f9702e6f3f3f8fe151e6d2b446067611acc9a1
Chris Johnson <chris@crisistextline.org>
2016-01-14 20:03:39 -0500

    Merge openshifts

---
+++ jobs/scheduling/MergeOpenShifts.js
@@ -0,0 +1,65 @@
+
+var CronJob = require('cron').CronJob;
+var WhenIWork = require('./base');
+var moment = require('moment');
+
+var wiw_date_format = 'ddd, DD MMM YYYY HH:mm:ss ZZ';
+
+new CronJob('0 0 * * * *', function () {
+    mergeOpenShifts();
+});
+
+mergeOpenShifts();
+
+function mergeOpenShifts() {
+    var query = {
+        include_allopen: true,
+        start: moment().add(-1, 'days').format(wiw_date_format),
+        end: moment().add(7, 'days').format(wiw_date_format),
+        location_id: global.config.locationID.regular_shifts
+    };
+
+    WhenIWork.get('shifts', query, function (data) {
+        var open_shifts = {};
+
+        for (var i in data.shifts) {
+            if (data.shifts[i].is_open) {
+                var shift = data.shifts[i];
+
+                if (typeof open_shifts[shift.start_time] == 'undefined') {
+                    open_shifts[shift.start_time] = [];
+                }
+
+                open_shifts[shift.start_time].push(shift);
+            }
+        }
+
+        for (var i in open_shifts) {
+            if (open_shifts[i].length > 1) {
+                var shift = open_shifts[i];
+                var max = -1;
+                var instances = 0;
+
+                for (var j in shift) {
+                    if (shift[j].instances == undefined || shift[j].instances == 0) {
+                        instances += 1;
+                    } else {
+                        instances += shift[j].instances;
+
+                        if (shift[j].instances > max) {
+                            max = shift[j].instances;
+                        }
+                    }
+                }
+
+                for (var j in shift) {
+                    if (shift[j].instances !== undefined && shift[j].instances == max) {
+                        WhenIWork.update('shifts/'+shift[j].id, {instances: instances});
+                    } else {
+                        WhenIWork.delete('shifts/'+shift[j].id);
+                    }
+                }
+            }
+        }
+    });
+}
