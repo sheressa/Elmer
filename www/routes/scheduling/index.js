c46e09548a2682182b41cb92c884b827089c318f
Tong Xiang <tong.xiang2@gmail.com>
2016-01-14 19:12:02 -0500

    add batch deletions to delete all shifts request

--- www/routes/scheduling/index.js
+++ www/routes/scheduling/index.js
@@ -37,15 +37,46 @@ router.get('/cancel-shift', function(req, res) {
                 var q = {
                     user_id: users[i].id,
                     start: moment().format('YYYY-MM-DD 00:00:00'),
-                    end: moment([2050]).format('YYYY-MM-DD HH:mm:ss'),
+                    end: moment().add(50, 'years').format('YYYY-MM-DD HH:mm:ss'),
                     unpublished: true,
                     location_id: global.config.locationID.regular_shifts
                 };
 
+                // Works to delete the normal volume of shifts associated with one user (2 shifts), recurred
+                // over 50 years. API will break if significantly larger volumes of shifts are attempted
+                // to be deleted. 
                 api.get('shifts', q, function (shifts) {
+                    var batchPayload = [];
                     for (var i in shifts.shifts) {
-                        api.update('shifts/'+shifts.shifts[i].id, {user_id: 0});
+
+                        var shift = shifts.shifts[i];
+                        // If the shift starts within a week, it's a shift that needs to be converted to an 
+                        // open shift because the open shift job has already run and passed that day. 
+                        if (Math.abs(moment().diff(moment(shift.start_time), 'days')) < global.config.time_interval.days_in_interval_to_repeat_open_shifts) {
+                            var reassignShiftToOpenAndRemoveNotesRequest = {
+                                "method": 'PUT',
+                                "url": "/2/shifts/" + shift.id,
+                                "params": {
+                                    user_id: 0, 
+                                    notes: ''
+                                }   
+                            }
+                            batchPayload.push(reassignShiftToOpenAndRemoveNotesRequest);
+                        }
+                        // Otherwise, we just delete the shift. 
+                        else {
+                            var shiftDeleteRequest = {
+                                "method": "delete",
+                                "url": "/2/shifts/" + shift.id,
+                                "params": {},
+                            };
+                            batchPayload.push(shiftDeleteRequest);
+                        }
                     }
+                    api.post('batch', batchPayload, function(response) {
+                        console.log(response);
+                    })
+
                     var api2 = new WhenIWork(global.config.wheniwork.api_key, email, global.config.wheniwork.default_password);
 
                     api2.post('users/autologin', function (data) {
@@ -110,4 +141,3 @@ function checkUser(email, first, last, callback) {
 }
 
 module.exports = router;
-
