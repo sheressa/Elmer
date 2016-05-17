70923aab33e9a80dd34cbefd687649de23958543
Tong Xiang <tong.xiang2@gmail.com>
2016-05-17 17:00:27 -0400

    jobs directory: convert tabs to spaces

--- jobs/scheduling/NotifyFirstShift.js
+++ jobs/scheduling/NotifyFirstShift.js
@@ -9,67 +9,67 @@ var mandrill_client = new mandrill.Mandrill(global.config.mandrill.api_key);
 var date_format = 'ddd, DD MMM YYYY HH:mm:ss ZZ';
 
 new CronJob(global.config.time_interval.notify_first_shift_cron_job_string, function () {
-    checkNewShifts();
+  checkNewShifts();
 }, null, true);
 
 checkNewShifts();
 
 function checkNewShifts() {
-    // TODO: Re-enable this.
-    return;
+  // TODO: Re-enable this.
+  return;
 
-    WhenIWork.get('users', {location_id: global.config.locationID.new_graduate}, function (users) {
-        var now = moment();
-        var template = fs.readFileSync('./email_templates/shift_welcome.txt', {encoding: 'utf-8'});
+  WhenIWork.get('users', {location_id: global.config.locationID.new_graduate}, function (users) {
+    var now = moment();
+    var template = fs.readFileSync('./email_templates/shift_welcome.txt', {encoding: 'utf-8'});
 
-        for (var i in users.users) {
-            var u = users.users[i];
-            var created = moment(u.created_at, date_format);
+    for (var i in users.users) {
+      var u = users.users[i];
+      var created = moment(u.created_at, date_format);
 
-            if (u.notes.indexOf('first_shift_notified') < 0 && now.diff(created, 'days') < global.config.days_to_search_for_new_users_who_have_scheduled_their_first_shift) {
-                var q = {
-                    user_id: u.id,
-                    start: moment().format(date_format),
-                    end: moment().add(global.config.days_to_search_for_new_users_who_have_scheduled_their_first_shift, 'days').format(date_format)
-                };
+      if (u.notes.indexOf('first_shift_notified') < 0 && now.diff(created, 'days') < global.config.days_to_search_for_new_users_who_have_scheduled_their_first_shift) {
+        var q = {
+          user_id: u.id,
+          start: moment().format(date_format),
+          end: moment().add(global.config.days_to_search_for_new_users_who_have_scheduled_their_first_shift, 'days').format(date_format)
+        };
 
-                WhenIWork.get('shifts', q, function (shifts) {
-                    var sent = false;
+        WhenIWork.get('shifts', q, function (shifts) {
+          var sent = false;
 
-                    for (var i in shifts.shifts) {
-                        var s = shifts.shifts[i];
-                        var created = moment(s.created_at, date_format);
+          for (var i in shifts.shifts) {
+            var s = shifts.shifts[i];
+            var created = moment(s.created_at, date_format);
 
-                        if (now.diff(created, 'minutes') <= interval && !sent) {
-                            var sent = true;
-                            var shift_start = moment(s.start_time, date_format).format('MMM D, YYYY HH:mm');
+            if (now.diff(created, 'minutes') <= interval && !sent) {
+              var sent = true;
+              var shift_start = moment(s.start_time, date_format).format('MMM D, YYYY HH:mm');
 
-                            var content = template.replace('%name', shifts.users[0].first_name).replace('%date', shift_start);
+              var content = template.replace('%name', shifts.users[0].first_name).replace('%date', shift_start);
 
-                            var message = {
-                                subject: 'Welcome aboard!',
-                                text: content,
-                                from_email: 'support@crisistextline.org',
-                                from_name: 'Crisis Text Line',
-                                to: [{
-                                    email: shifts.users[0].email,
-                                    name: shifts.users[0].first_name + ' ' + shifts.users[0].last_name,
-                                    type: 'to'
-                                }],
-                                headers: {
-                                    "Reply-To": "shannon@crisistextline.org",
-                                }
-                            };
+              var message = {
+                  subject: 'Welcome aboard!',
+                  text: content,
+                  from_email: 'support@crisistextline.org',
+                  from_name: 'Crisis Text Line',
+                  to: [{
+                      email: shifts.users[0].email,
+                      name: shifts.users[0].first_name + ' ' + shifts.users[0].last_name,
+                      type: 'to'
+                  }],
+                  headers: {
+                      "Reply-To": "shannon@crisistextline.org",
+                  }
+              };
 
-                            mandrill_client.messages.send({message: message, key: 'first_shift_scheduled'}, function (res) {
-                                console.log(res);
-                            });
+              mandrill_client.messages.send({message: message, key: 'first_shift_scheduled'}, function (res) {
+                  console.log(res);
+              });
 
-                            WhenIWork.update('users/'+shifts.users[0].id, {notes: shifts.users[0].notes + 'first_shift_notified' + "\n"});
-                        }
-                    }
-                });
+              WhenIWork.update('users/'+shifts.users[0].id, {notes: shifts.users[0].notes + 'first_shift_notified' + "\n"});
             }
-        }
-    });
+          }
+        });
+      }
+    }
+  });
 }
--- jobs/scheduling/TimeOffRequests.js
+++ jobs/scheduling/TimeOffRequests.js
@@ -14,86 +14,86 @@ new CronJob(global.config.time_interval.time_off_requests_cron_job_string, funct
 handleTimeOffRequests();
 
 function handleTimeOffRequests() {
-    //Using moment.js to format time as WIW expects
-    var startDateToRetrieveRequests = moment().format(date_format);
-    var endDateToRetrieveRequests = moment()
-        .add(global.config.time_interval.months_to_search_for_time_off_requests, 'months')
-        .format(date_format);
-    var timeOffSearchParams = {
-        "start": startDateToRetrieveRequests,
-        "end": endDateToRetrieveRequests,
-    };
+  //Using moment.js to format time as WIW expects
+  var startDateToRetrieveRequests = moment().format(date_format);
+  var endDateToRetrieveRequests = moment()
+    .add(global.config.time_interval.months_to_search_for_time_off_requests, 'months')
+    .format(date_format);
+  var timeOffSearchParams = {
+    "start": startDateToRetrieveRequests,
+    "end": endDateToRetrieveRequests,
+  };
 
-    //Get all time off requests within timeOffSearchParams
-    WhenIWork.get('requests', timeOffSearchParams, function(response) {
-        var allRequests = response.requests;
+  //Get all time off requests within timeOffSearchParams
+  WhenIWork.get('requests', timeOffSearchParams, function(response) {
+    var allRequests = response.requests;
 
-        //Filter requests to pending requests only
-        var newRequests = allRequests.filter(function(request) {
-            return request.status === 0;
-        });
+    //Filter requests to pending requests only
+    var newRequests = allRequests.filter(function(request) {
+      return request.status === 0;
+    });
 
-        //For each pending request, look for any shifts that fall within the time off request
-        newRequests.forEach(function(request) {
-            var shiftSearchParams = {
-                "start": request.start_time,
-                "end": request.end_time,
-                "user_id": request.user_id,
-                "location_id": [global.config.locationID.regular_shifts, global.config.locationID.makeup_and_extra_shifts],
-                "unpublished": true
+    //For each pending request, look for any shifts that fall within the time off request
+    newRequests.forEach(function(request) {
+      var shiftSearchParams = {
+        "start": request.start_time,
+        "end": request.end_time,
+        "user_id": request.user_id,
+        "location_id": [global.config.locationID.regular_shifts, global.config.locationID.makeup_and_extra_shifts],
+        "unpublished": true
+      };
+
+      WhenIWork.get('shifts', shiftSearchParams, function(response) {
+
+        // Status code `2` represents approved requests.
+        var timeOffApprovalRequest = {
+          "method": "put",
+          "url": "/2/requests/" + request.id,
+          "params": {
+            "status": 2
+          }
+        };
+
+        var batchPayload = [timeOffApprovalRequest];
+
+        if (!response.shifts || response.shifts.length === 0) {
+          console.log('No shifts found that fall within the range of that timeoff request, but we\'re still approving that request.');
+        }
+        else {
+          //For each shift, create a delete request and an open shift to replace it
+          response.shifts.forEach(function(shift) {
+            var shiftDeleteRequest = {
+              "method": "delete",
+              "url": "/2/shifts/" + shift.id,
+              "params": {},
             };
 
-            WhenIWork.get('shifts', shiftSearchParams, function(response) {
+            batchPayload.push(shiftDeleteRequest);
 
-                // Status code `2` represents approved requests.
-                var timeOffApprovalRequest = {
-                    "method": "put",
-                    "url": "/2/requests/" + request.id,
-                    "params": {
-                        "status": 2
-                    }
-                };
+            var params = {
+              "start_time": shift.start_time,
+              "end_time": shift.end_time,
+              "notes": "SHIFT COVERAGE",
+              "published": true,
+              "location_id": global.config.locationID
+                                   .makeup_and_extra_shifts,
+            };
 
-                var batchPayload = [timeOffApprovalRequest];
+            params = returnColorizedShift(params, shift.start_time, true);
 
-                if (!response.shifts || response.shifts.length === 0) {
-                    console.log('No shifts found that fall within the range of that timeoff request, but we\'re still approving that request.');
-                }
-                else {
-                    //For each shift, create a delete request and an open shift to replace it
-                    response.shifts.forEach(function(shift) {
-                        var shiftDeleteRequest = {
-                            "method": "delete",
-                            "url": "/2/shifts/" + shift.id,
-                            "params": {},
-                        };
-
-                        batchPayload.push(shiftDeleteRequest);
-
-                        var params = {
-                                "start_time": shift.start_time,
-                                "end_time": shift.end_time,
-                                "notes": "SHIFT COVERAGE",
-                                "published": true,
-                                "location_id": global.config.locationID
-                                                     .makeup_and_extra_shifts,
-                            };
-
-                        params = returnColorizedShift(params, shift.start_time, true);
-
-                        var newOpenShiftRequest = {
-                            "method": "post",
-                            "url": "/2/shifts",
-                            "params": params
-                        };
-                        batchPayload.push(newOpenShiftRequest);
-                    });
-                }
-                //Send the time off request approval and all shift deletions/open shift creations to batch
-                WhenIWork.post('batch', batchPayload, function(response) {
-                    console.log(response);
-                });
-            });
+            var newOpenShiftRequest = {
+              "method": "post",
+              "url": "/2/shifts",
+              "params": params
+            };
+            batchPayload.push(newOpenShiftRequest);
+          });
+        }
+        //Send the time off request approval and all shift deletions/open shift creations to batch
+        WhenIWork.post('batch', batchPayload, function(response) {
+          console.log(response);
         });
+      });
     });
+  });
 }
