9edd8aca152bea21561e234be8de9ac007b3f468
John Rauschenberg <john@Johns-MacBook-Pro.local>
2016-05-26 10:45:47 -0400

    Added tests for sorting regular and makeup shifts.

--- test/retrieveAndRenderShiftsToDelete.js
+++ test/retrieveAndRenderShiftsToDelete.js
@@ -14,8 +14,6 @@ describe('retrieveAndRenderShiftsToDelete should retrieve shifts and render them
 
   // Set up the API mock to send back data that we can pass in to our test.
   beforeEach(function() {
-    console.log('running setup stuff in beforeEach');
-
     var apiMocker = nock('https://api.wheniwork.com/2')
           .get('/users')
           .query(true)
@@ -34,7 +32,6 @@ describe('retrieveAndRenderShiftsToDelete should retrieve shifts and render them
   describe('Shift retrieval for a user', function() {
 
     it('should retrieve shifts belonging to a user', function (done) {
-      // var api = nock('https://api.wheniwork.com/2');
 
       // Dummy express request object
       var request = {
@@ -51,7 +48,7 @@ describe('retrieveAndRenderShiftsToDelete should retrieve shifts and render them
         },
 
         render: function(routeToRender, templateData) {
-          assert.equal(templateData.regularShifts.length, 4);
+          assert.equal(templateData.regularShifts.length, 2);
           done();
         }
       };
@@ -59,6 +56,93 @@ describe('retrieveAndRenderShiftsToDelete should retrieve shifts and render them
       retrieveAndRenderShiftsToDelete(request, response, api);
 
     });
+
+    it('should give a 403 (access denied) if helper validation fails', function (done) {
+
+      // Dummy express request object
+      var request = {
+        query: {
+          email: 'test@test.com',
+          token: 'x'
+        }
+      };
+
+      // Dummy Express response object
+      var response = {
+        status: function(code) {
+          assert.equal(code, 403);
+          return this;
+        },
+        send: function(message) {
+          assert.equal(message, 'Access denied.');
+          done();
+        }
+
+      };
+
+      retrieveAndRenderShiftsToDelete(request, response, api);
+
+    });
+
+    it('should sort regularShifts by when they occur on the weekly calendar', function (done) {
+
+      // Dummy express request object
+      var request = {
+        query: {
+          email: 'amudantest@test.com',
+          token: '9365583ac27c52684eb6efb8e9374c04823dce59'
+        }
+      };
+
+      // Dummy Express response object
+      var response = {
+        status: function(code) {
+          assert.equal(code, 403);
+          return this;
+        },
+
+        render: function(routeToRender, templateData) {
+          assert.equal(templateData.regularShifts[0].id, 277119037);
+          done();
+        }
+
+
+      };
+
+      retrieveAndRenderShiftsToDelete(request, response, api);
+
+    });
+
+    it('should sort makeupShifts by when they occur on the weekly calendar', function (done) {
+
+      // Dummy express request object
+      var request = {
+        query: {
+          email: 'amudantest@test.com',
+          token: '9365583ac27c52684eb6efb8e9374c04823dce59'
+        }
+      };
+
+      // Dummy Express response object
+      var response = {
+        status: function(code) {
+          assert.equal(code, 403);
+          return this;
+        },
+
+        render: function(routeToRender, templateData) {
+          assert.equal(templateData.makeupShifts[0].id, 281883643);
+          done();
+        }
+
+
+      };
+
+      retrieveAndRenderShiftsToDelete(request, response, api);
+
+    });
+
+
 
   });
 
