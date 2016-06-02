e66e987e517724851ed8f0c7337d2f3bfafed968
Tong Xiang <tong.xiang2@gmail.com>
2016-06-02 18:42:46 -0400

    add two week shift open window revisions for MergeOpenShifts, NotifyFirstShift, NotifyMoreShifts jobs

--- jobs/scheduling/MergeOpenShifts.js
+++ jobs/scheduling/MergeOpenShifts.js
@@ -1,11 +1,10 @@
-
 var CronJob = require('cron').CronJob;
 var WhenIWork = require('./base');
 var returnColorizedShift = require('../../lib/ColorizeShift').go;
 
 var wiw_date_format = 'ddd, DD MMM YYYY HH:mm:ss ZZ';
 
-new CronJob(global.config.time_interval.open_shifts, function () {
+new CronJob(config.time_interval.open_shifts, function () {
     mergeOpenShifts();
 }, null, true);
 
@@ -15,26 +14,25 @@ function mergeOpenShifts() {
   var query = {
     include_allopen: true,
     start: '-1 day',
-    end: '+7 days',
-    location_id: [ global.config.locationID.regular_shifts, global.config.locationID.makeup_and_extra_shifts ]
+    end: '+' + config.time_interval.days_of_open_shift_display + ' days',
+    location_id: [ config.locationID.regular_shifts, config.locationID.makeup_and_extra_shifts ]
   };
 
   WhenIWork.get('shifts', query, function (data) {
-    var openRegShifts = {}
-      , openMakShifts = {}
-      , shift
-      , batchPayload = []
-      ;
+    var openRegShifts = {};
+    var openMakShifts = {};
+    var shift;
+    var batchPayload = [];
 
     for (var i in data.shifts) {
       shift = data.shifts[i];
-      if (shift.is_open && shift.location_id == global.config.locationID.regular_shifts) {
+      if (shift.is_open && shift.location_id == config.locationID.regular_shifts) {
         if (typeof openRegShifts[shift.start_time] == 'undefined') {
           openRegShifts[shift.start_time] = [];
         }
         openRegShifts[shift.start_time].push(shift);
       }
-      else if (shift.is_open && shift.location_id == global.config.locationID.makeup_and_extra_shifts) {
+      else if (shift.is_open && shift.location_id == config.locationID.makeup_and_extra_shifts) {
         if (typeof openMakShifts[shift.start_time] == 'undefined') {
           openMakShifts[shift.start_time] = [];
         }
@@ -42,29 +40,28 @@ function mergeOpenShifts() {
       }
     }
 
-    for (key in openRegShifts) {
+    for (var key in openRegShifts) {
       batchPayload = makeBatchPayloadRequestsToMergeOpenShifts(openRegShifts[key], batchPayload);
     }
 
-    for (key in openMakShifts) {
+    for (var key in openMakShifts) {
       batchPayload = makeBatchPayloadRequestsToMergeOpenShifts(openMakShifts[key], batchPayload);
     }
 
     WhenIWork.post('batch', batchPayload, function(response) {
       CONSOLE_WITH_TIME('Response from merge shift batch payload request: ', batchPayload);
-    })
+    });
   });
 }
 
 function makeBatchPayloadRequestsToMergeOpenShifts(arrayOfShiftsForSameTimeInt, batchPayload) {
   if (arrayOfShiftsForSameTimeInt && arrayOfShiftsForSameTimeInt.length > 1) {
-    var max = -1
-      , instances = 0
-      , remainingShiftUpdated = false
-      ;
+    var max = -1;
+    var instances = 0;
+    var remainingShiftUpdated = false;
 
     for (var j in arrayOfShiftsForSameTimeInt) {
-      if (arrayOfShiftsForSameTimeInt[j].instances == undefined || arrayOfShiftsForSameTimeInt[j].instances == 0) {
+      if (arrayOfShiftsForSameTimeInt[j].instances === undefined || arrayOfShiftsForSameTimeInt[j].instances === 0) {
         instances += 1;
       } else {
         instances += arrayOfShiftsForSameTimeInt[j].instances;
@@ -78,7 +75,7 @@ function makeBatchPayloadRequestsToMergeOpenShifts(arrayOfShiftsForSameTimeInt, 
     for (var j in arrayOfShiftsForSameTimeInt) {
       if (arrayOfShiftsForSameTimeInt[j].instances !== undefined && arrayOfShiftsForSameTimeInt[j].instances == max && !remainingShiftUpdated) {
         var update = {instances: instances};
-        var isMakeupShift = arrayOfShiftsForSameTimeInt[j].location_id === global.config.locationID.makeup_and_extra_shifts;
+        var isMakeupShift = arrayOfShiftsForSameTimeInt[j].location_id === config.locationID.makeup_and_extra_shifts;
         update = returnColorizedShift(update, arrayOfShiftsForSameTimeInt[j].start_time, isMakeupShift);
 
 
@@ -86,7 +83,7 @@ function makeBatchPayloadRequestsToMergeOpenShifts(arrayOfShiftsForSameTimeInt, 
           'method': 'PUT',
           'url': '/2/shifts/' + arrayOfShiftsForSameTimeInt[j].id,
           'params': update
-        }
+        };
 
         batchPayload.push(shiftUpdateRequest);
         remainingShiftUpdated = true;
@@ -95,7 +92,7 @@ function makeBatchPayloadRequestsToMergeOpenShifts(arrayOfShiftsForSameTimeInt, 
           'method': 'delete',
           'url': '/2/shifts/' + arrayOfShiftsForSameTimeInt[j].id,
           'params': {}
-        }
+        };
         batchPayload.push(shiftDeleteRequest);
       }
     }
