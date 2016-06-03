363e316339ca61f69200419b0690447b2d3ba56e
Tong Xiang <tong.xiang2@gmail.com>
2016-06-03 10:56:59 -0400

    add changes to recurOpenShifts to support opening up a two week open shift signup window

--- jobs/scheduling/RecurOpenShifts.js
+++ jobs/scheduling/RecurOpenShifts.js
@@ -18,6 +18,7 @@ function runJob() {
 // run it once on boot
 runJob();
 
+// Recurs open shifts one and two weeks in the future.
 function recurOpenShifts(now) {
   if (now.hours() % 2 == 1) {
     CONSOLE_WITH_TIME('running at an odd hour. abort.');
@@ -28,64 +29,22 @@ function recurOpenShifts(now) {
     // Because we're making async calls in a loop, we pass targetTime through callback
     // so that it's defined locally for each scope.
     var targetTime = now.clone().add(i * -2, 'hours');
-    findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTime, function(extraOpenShiftsToDelete, correctNumberOfShiftsToSet, targetTime) {
-      var batchPayload = [];
-
-      // If we don't need to add any new open shifts, we return.
-      if (correctNumberOfShiftsToSet === 0) {
-        CONSOLE_WITH_TIME('No open shifts need to be added for time: ', targetTime.toString());
-        return;
-      }
-      // If we need to add open shifts
-      else if (correctNumberOfShiftsToSet > 0) {
-        var newOpenShiftParams = {
-          start_time: targetTime.clone().minute(0).second(0).add(1, 'week').format(WIWDateFormat),
-          end_time: targetTime.clone().minute(0).second(0).add(2, 'hour').add(1, 'week').format(WIWDateFormat),
-          location_id: global.config.locationID.regular_shifts,
-          instances: correctNumberOfShiftsToSet,
-          published: true
-        };
-        newOpenShiftParams = colorizeShift(newOpenShiftParams);
-
-        var newOpenShiftRequest = {
-          method: "post",
-          url: "/2/shifts",
-          params: newOpenShiftParams
-        };
-        batchPayload.push(newOpenShiftRequest);
-      }
-      /**
-        Batch delete the invalid open shifts in two cases: 1) correctNumberOfShiftsToSet > 0; we need to add open shifts
-        and 2) correctNumberOfShiftsToSet < 0; the number of occupied shifts is currently greater than the total number
-        of open shifts for that block. In BOTH cases, we need to delete all old open shifts.
-      **/
-
-      extraOpenShiftsToDelete.forEach(function(shiftID) {
-        var shiftDeleteRequest = {
-            method: "delete",
-            url: "/2/shifts/" + shiftID,
-            params: {}
-        };
-        batchPayload.push(shiftDeleteRequest);
-      });
-
-      if (correctNumberOfShiftsToSet < 0) { correctNumberOfShiftsToSet = 'no'; }
-      CONSOLE_WITH_TIME('Adding ', correctNumberOfShiftsToSet, ' open shifts to the time: ', targetTime.toString(), '. Deleting incorrect count of open shifts--their shift IDs: ', extraOpenShiftsToDelete);
-      WhenIWork.post('batch', batchPayload);
-    })
+    findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTime, 1, incrementFutureOpenShiftsUpOrDown);
+    findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTime, 2, incrementFutureOpenShiftsUpOrDown);
   }
 }
+
 /**
-  Checks if open shifts are already present a week from the targetTime
+  Checks if open shifts are already present weeksFromNowToCheck number of weeks from the targetTime
   Callback params: callback(extraOpenShiftsToDelete, correctNumberOfShiftsToSet, targetTimeMomentObj);
 **/
-function findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTimeMomentObj, callback) {
-  var extraOpenShiftsToDelete = []
-    , countOfOccupiedShifts = 0
-    , countOfOpenShifts = 0
-    , targetTimeMomentObj = targetTimeMomentObj
-    , shift
-    ;
+function findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTimeMomentObj, weeksFromNowToCheck, callback) {
+  var extraOpenShiftsToDelete = [];
+  var countOfOccupiedShifts = 0;
+  var countOfOpenShifts = 0;
+  var shift;
+
+  targetTimeMomentObj = targetTimeMomentObj;
   /*
     Querying for shifts starting at 8pm produces weird, buggy behavior from the WIW API.
     Guess: 8pm is the dividing line between GMT days. So if we query for shifts starting a
@@ -93,8 +52,8 @@ function findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTimeMomentObj, c
     is precisely on the hour.
   */
   var filter = {
-    start: targetTimeMomentObj.clone().add(1, 'week').format(shiftQueryDateFormat),
-    end: targetTimeMomentObj.clone().add(1, 'minute').add(1, 'week').format(shiftQueryDateFormat),
+    start: targetTimeMomentObj.clone().add(weeksFromNowToCheck, 'week').format(shiftQueryDateFormat),
+    end: targetTimeMomentObj.clone().add(1, 'minute').add(weeksFromNowToCheck, 'week').format(shiftQueryDateFormat),
     include_allopen: true,
     location_id: global.config.locationID.regular_shifts
   };
@@ -117,10 +76,60 @@ function findExtraOpenShiftsToDeleteAndOccupiedShiftCount(targetTimeMomentObj, c
     }
 
     var correctNumberOfShiftsToSet = returnMaxOpenShiftCountForTime(targetTimeMomentObj.clone()) - countOfOccupiedShifts;
-    CONSOLE_WITH_TIME('Found ', countOfOpenShifts, ' open shifts, ', countOfOccupiedShifts, ' occupied shifts found for a time where we expect ', returnMaxOpenShiftCountForTime(targetTimeMomentObj.clone()), ' open shifts. Time: ', targetTimeMomentObj.toString());
-    callback(extraOpenShiftsToDelete, correctNumberOfShiftsToSet, targetTimeMomentObj);
+    CONSOLE_WITH_TIME('Found ', countOfOpenShifts, ' open shifts, ', countOfOccupiedShifts, ' occupied shifts found for a time ', weeksFromNowToCheck, 'weeks from now where we expect ', returnMaxOpenShiftCountForTime(targetTimeMomentObj.clone()), ' open shifts. Time: ', targetTimeMomentObj.toString());
+    callback(extraOpenShiftsToDelete, correctNumberOfShiftsToSet, weeksFromNowToCheck, targetTimeMomentObj);
     return;
   });
+}
+
+/**
+  Based on the shift information passed to this function by findExtraOpenShiftsToDeleteAndOccupiedShiftCount,
+  this function adds open shifts or delete open shifts to match the proper number.
+**/
+function incrementFutureOpenShiftsUpOrDown(extraOpenShiftsToDelete, correctNumberOfShiftsToSet, weeksFromNowToCheck, targetTime) {
+  var batchPayload = [];
+
+  // If we don't need to add any new open shifts, we return.
+  if (correctNumberOfShiftsToSet === 0) {
+    CONSOLE_WITH_TIME('No open shifts need to be added for time: ', targetTime.toString());
+    return;
+  }
+  // If we need to add open shifts
+  else if (correctNumberOfShiftsToSet > 0) {
+    var newOpenShiftParams = {
+      start_time: targetTime.clone().minute(0).second(0).add(weeksFromNowToCheck, 'weeks').format(WIWDateFormat),
+      end_time: targetTime.clone().minute(0).second(0).add(2, 'hour').add(weeksFromNowToCheck, 'weeks').format(WIWDateFormat),
+      location_id: global.config.locationID.regular_shifts,
+      instances: correctNumberOfShiftsToSet,
+      published: true
+    };
+    newOpenShiftParams = colorizeShift(newOpenShiftParams);
+
+    var newOpenShiftRequest = {
+      method: "post",
+      url: "/2/shifts",
+      params: newOpenShiftParams
+    };
+    batchPayload.push(newOpenShiftRequest);
+  }
+
+  /**
+    Batch delete the invalid open shifts in two cases: 1) correctNumberOfShiftsToSet > 0; we need to add open shifts
+    and 2) correctNumberOfShiftsToSet < 0; the number of occupied shifts is currently greater than the total number
+    of open shifts for that block. In BOTH cases, we need to delete all old open shifts.
+  **/
+  extraOpenShiftsToDelete.forEach(function(shiftID) {
+    var shiftDeleteRequest = {
+      method: "delete",
+      url: "/2/shifts/" + shiftID,
+      params: {}
+    };
+    batchPayload.push(shiftDeleteRequest);
+  });
+
+  if (correctNumberOfShiftsToSet < 0) { correctNumberOfShiftsToSet = 'no'; }
+  CONSOLE_WITH_TIME('Adding ', correctNumberOfShiftsToSet, ' open shifts to the time: ', targetTime.toString(), ' . Deleting incorrect count of open shifts--their shift IDs: ', extraOpenShiftsToDelete);
+  WhenIWork.post('batch', batchPayload);
 }
 
 function returnMaxOpenShiftCountForTime(targetTimeMomentObj) {
@@ -134,5 +143,6 @@ module.exports = {
   recurOpenShifts: recurOpenShifts,
   returnMaxOpenShiftCountForTime: returnMaxOpenShiftCountForTime,
   findExtraOpenShiftsToDeleteAndOccupiedShiftCount: findExtraOpenShiftsToDeleteAndOccupiedShiftCount,
+  incrementFutureOpenShiftsUpOrDown: incrementFutureOpenShiftsUpOrDown,
   cronJob: cronJob
 };
