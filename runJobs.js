d4a9aaaae1ab1114144c0f5922bb0e2066ab2d0b
Assaf <apackin@gmail.com>
2016-06-15 13:52:54 -0400

    modify runJobs so KEYS are imported first

--- runJobs.js
+++ runJobs.js
@@ -1,9 +1,10 @@
-global.CONFIG = require('./config');
 global.KEYS = require('./keys.js');
+global.CONFIG = require('./config');
 require('newrelic');
 
 if (process.env.NODE_ENV !== 'production') {
 	CONFIG.locationID.regular_shifts = CONFIG.locationID.test;
+  CONFIG.locationID.new_graduate = CONFIG.locationID.test;
   CONFIG.locationID.makeup_and_extra_shifts = CONFIG.locationID.test2;
 }
 
