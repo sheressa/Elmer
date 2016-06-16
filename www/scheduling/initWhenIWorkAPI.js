95cba2c1ee44674e3e38a3410ccb49fa00e0a685
John Rauschenberg <john@Johns-MacBook-Pro.local>
2016-06-16 16:11:18 -0400

    Refactored test base.js slightly to allow post and update functions to still allow callbacks to run (otherwise some tests break). Also slightly refactored initWhenIWorkAPI to allow for being called in test environments.

--- www/scheduling/initWhenIWorkAPI.js
+++ www/scheduling/initWhenIWorkAPI.js
@@ -1,6 +1,6 @@
 var WhenIWork = require.main.require('wheniwork-unofficial');
 
 // Node will re-use a module once you import it once, so this API will not initialized twice.
-var api = new WhenIWork(KEYS.wheniwork.api_key, KEYS.wheniwork.username, KEYS.wheniwork.password);
+var api = process.env.NODE_ENV === 'test' ? require('./test/helpers/base') : new WhenIWork(KEYS.wheniwork.api_key, KEYS.wheniwork.username, KEYS.wheniwork.password);
 
 module.exports = api;
