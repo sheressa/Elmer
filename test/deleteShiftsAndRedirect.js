f77dd876cc7f3c71648ea1430cf5aa9fcb28da40
Mariya B <mb2283@cornell.edu>
2016-07-05 11:45:38 -0400

    changed sample data reference

--- test/deleteShiftsAndRedirect.js
+++ test/deleteShiftsAndRedirect.js
@@ -1,11 +1,29 @@
-var api = require('./helpers/base.js');
-var assert = require('assert')
-  , sampleData = require('./sampleData')
-  var deleteShiftsAndRedirect = require(CONFIG.root_dir + '/www/scheduling/shifts/controllers/deleteShiftsAndRedirect')
+global.KEYS = require('../keys.js');
+global.CONFIG = require('../config.js');
+
+var api = require('./helpers/base.js'),
+  assert = require('assert'), 
+  sampleData = require('../sample_data/sampleData.js'),
+  deleteShiftsAndRedirect = require(CONFIG.root_dir + '/www/scheduling/shifts/controllers/deleteShiftsAndRedirect')
   ;
 describe('delete shifts and redirect', function() {
   var userEmail = 'amudantest@test.com';
   var userToken = '9365583ac27c52684eb6efb8e9374c04823dce59';
+  // Dummy Express response object
+  var response = {
+    status: function(code) {
+      console.log('Response code: ' + code);
+      return this;
+    },
+    json: function(response) {
+    }
+  };
+  var request = {
+    query: {
+      email: userEmail,
+      token: 'x'
+    }
+  };
 
   describe('shift deletion', function() {
 
@@ -19,99 +37,46 @@ describe('delete shifts and redirect', function() {
         }
       };
       // Dummy Express response object
-      var response = {
-        status: function(code) {
-          return this;
-        },
-
-        json: function(response) {
-        },
-
-        send: function(message) {
+        response.send =  function(message) {
           assert.equal(message, 'Access denied.');
           done();
-        },
-
-        render: function(routeToRender, templateData) {
-          if (templateData.regularShifts.length > 0) {
-            done();
-          }
-        }
-      };
-
+        };
       deleteShiftsAndRedirect(request, response, api);
     });
 
     it('finds a regular shift to be deleted', function (done) {
 
       // Dummy express request object
-      var request = {
-        query: {
-          email: userEmail,
-          token: userToken,
-          regShift284947012: 'on'
-        }
-      };
+       request.query.token = userToken,
+       request.query.regShift284947012 = 'on';
       // Dummy Express response object
-      var response = {
-        status: function(code) {
-          console.log('Response code: ' + code);
-          return this;
-        },
-
-        json: function(response) {
+        response.json =  function(response) {
           assert.equal(response.redirect, '/scheduling/shifts/delete-success?deletedShiftInformation={"regShifts":{},"makShifts":{}}&email=amudantest%40test.com&token=9365583ac27c52684eb6efb8e9374c04823dce59&url=https://app.wheniwork.com/&');
           done();
-        },
+        };
 
-        send: function(message) {
+        response.send =  function(message) {
           assert.equal(message, 'Access denied.');
           done();
-        },
-
-        render: function(routeToRender, templateData) {
-          if (templateData.regularShifts.length > 0) {
-            done();
-          }
-        }
-      };
-
+        };
       deleteShiftsAndRedirect(request, response, api);
     });
 
     it('finds a makeup shift to be deleted', function (done) {
 
       // Dummy express request object
-      var request = {
-        query: {
-          email: userEmail,
-          token: userToken,
-          makShift284947012: 'on'
-        }
-      };
+        request.query.token = userToken,
+        request.query.makShift281883880 = 'on';
       // Dummy Express response object
-      var response = {
-        status: function(code) {
-          console.log('Response code: ' + code);
-          return this;
-        },
-
-        json: function(response) {
+        response.json =  function(response) {
           assert.equal(response.redirect, '/scheduling/shifts/delete-success?deletedShiftInformation={"regShifts":{},"makShifts":{}}&email=amudantest%40test.com&token=9365583ac27c52684eb6efb8e9374c04823dce59&url=https://app.wheniwork.com/&');
           done();
-        },
+        };
 
-        send: function(message) {
+        response.send = function(message) {
           assert.equal(message, 'Access denied.');
           done();
-        },
-
-        render: function(routeToRender, templateData) {
-          if (templateData.regularShifts.length > 0) {
-            done();
-          }
-        }
-      };
+        };
       deleteShiftsAndRedirect(request, response, api);
     });
   });
