5733236d60afb373b88c2307aefc6fc5b8ba213b
Mariya B <mb2283@cornell.edu>
2016-07-19 15:27:35 -0400

    added catch blocks and fixed semicolons

--- jobs/training/pollCanvasForGraduatedUsersThenCreatePlatformAccount.js
+++ jobs/training/pollCanvasForGraduatedUsersThenCreatePlatformAccount.js
@@ -5,7 +5,6 @@ var internalRequest = require('request');
 var moment = require('moment');
 var throttler = require('throttled-request')(internalRequest);
 var CronJob = require('cron').CronJob;
-
 var SLACK_CHANNEL = '#graduates';
 var emailsDone = [];
 
@@ -23,39 +22,45 @@ pollCanvasForGraduatedUsersThenCreatePlatformAccount();
 // Polls Canvas for people who’ve passed the "Graduation" course
 function pollCanvasForGraduatedUsersThenCreatePlatformAccount() {
   request('accounts/' + KEYS.canvas.accountID + '/courses', 'GET')
-    .then(function (courses) {
-      courses.forEach(function (course) {
-        /**
-          Excluding courses that we don’t need to parse for graduations, because they’re either test courses or not related to CTL training.
-        **/
-        if (KEYS.canvas.coursesWeDoNotParseForGraduatedUsers.indexOf(course.id) >= 0) return;
-        request('/audit/grade_change/courses/'+course.id, 'GET')
-        .then(function(gradebook){
+  .then(function (courses) {
+    courses.forEach(function (course) {
+    /**
+      Excluding courses that we don’t need to parse for graduations, because they’re either test courses or not related to CTL training.
+    **/
+      if (KEYS.canvas.coursesWeDoNotParseForGraduatedUsers.indexOf(course.id) >= 0) return;
+      request('/audit/grade_change/courses/'+course.id, 'GET')
+      .then(function(gradebook){
           var assignments = gradebook.linked.assignments;
           var platformReadyId;
           var finalExamId;
-          for(var i = 0; i<assignments.length; i++){
-              if (assignments[i].name.indexOf(CONFIG.canvas.assignments.finalExam)>=0){
-                finalExamId = assignments[i].quiz_id; 
-              }
-              if (assignments[i].name.indexOf(CONFIG.canvas.assignments.platformReady)>=0){
-                platformReadyId = assignments[i].id; 
-              }
-              if(i==assignments.length-1 && finalExamId && platformReadyId){
-                var ids = {courseId: assignments[i].course_id, finalExamId: finalExamId, platformReadyId: platformReadyId}
-                platformReadyChecker(gradebook, ids)
-                return;
-              }
+        for(var i = 0; i<assignments.length; i++){
+          if (assignments[i].name.indexOf(CONFIG.canvas.assignments.finalExam)>=0){
+            finalExamId = assignments[i].quiz_id; 
           }
-        })
+          if (assignments[i].name.indexOf(CONFIG.canvas.assignments.platformReady)>=0){
+            platformReadyId = assignments[i].id; 
+          }
+          if(i===assignments.length-1 && finalExamId && platformReadyId){
+            var ids = {courseId: assignments[i].course_id, finalExamId: finalExamId, platformReadyId: platformReadyId}
+            platformReadyChecker(gradebook, ids);
+            return;
+          }
+        }
       })
-    })  
-};
+      .catch(function (error) {
+        CONSOLE_WITH_TIME('Call to Canvas to get grade logs for course ' +course.id+' has failed, error: ', error);
+        });
+      });
+  })
+  .catch(function (error) {
+    CONSOLE_WITH_TIME('Call to Canvas to get courses has failed, error: ', error);
+  });  
+}
 
 //checks whether a student is platform ready
 function platformReadyChecker(gradebook, ids){
   var events = gradebook.events;
-  var startDate = moment().subtract(6, "hours");
+  var startDate = moment().subtract(6, 'hours');
   var platformReady = {};
   //check if a user graduated within the last 6 hours 
   events.forEach(function(event){
@@ -65,38 +70,39 @@ function platformReadyChecker(gradebook, ids){
   });
   //check if the users got 85+ on final exam
   finalExamChecker(platformReady, ids.finalExamId, ids.courseId, gradebook.linked.users);
-};
+}
 
 //checks whether a student got 85+ on final exam
 function finalExamChecker(studentObj, finalExamId, courseId, users){
-  request('courses/' +courseId+ '/quizzes/' + finalExamId+'/submissions', 'GET', ['per_page=100'])
-    .then(function (quizzes) {
-      quizzes.quiz_submissions.forEach(function(quiz){
+  request('courses/' +courseId+ '/quizzes/' + finalExamId+'/submissions', 'GET')
+  .then(function (quizzes) {
+    quizzes.quiz_submissions.forEach(function(quiz){
       if(studentObj[quiz.user_id] && quiz.score<85) {
           delete studentObj[quiz.user_id];
         }
-      });
-      if(studentObj){
-        return studentObj;
-      }
-    })
-    .then(function(students){
-      users.forEach(function(user){
-        if (students[user.id]) createEmail(user);
-      });
     });
+      return studentObj;
+  })
+  .then(function(students){
+    users.forEach(function(user){
+      if (students[user.id]) createEmail(user);
+    });
+  })
+  .catch(function (error) {
+    CONSOLE_WITH_TIME('Call to Canvas for quiz logs has failed, error: ', error);
+  });
 }
 
 // creates the body of a platform post request
 function createEmail(student) {
-  CONSOLE_WITH_TIME('Graduating user ', student)
-    var name = student.name.split(' ');
-    var body = {
-      firstName: name[0],
-      lastName: name[1],
-      email: student.login_id
-    };
-    updatePlatform(body);
+  CONSOLE_WITH_TIME('Graduating user ', student);
+  var name = student.name.split(' ');
+  var body = {
+    firstName: name[0],
+    lastName: name[1],
+    email: student.login_id
+  };
+  updatePlatform(body);
     
 }
 // Creates a platform account for users who have passed the "Graduation" course
@@ -144,7 +150,7 @@ function request(url, method, params) {
   if (!params) {
     params = [];
   }
-  params.push("per_page=1000");
+  params.push('per_page=1000');
 
   url = 'https://crisistextline.instructure.com/api/v1/' + url;
 
