2bc5dca8848e03b6b5c0906664597db1d29335a7
Assaf <apackin@gmail.com>
2016-08-17 14:25:00 -0400

    Create route for typeform submissions and post to Canvas submissions

--- test/helpers/checkHTTPResponses.js
+++ test/helpers/checkHTTPResponses.js
@@ -8,7 +8,7 @@
 const moment = require('moment-timezone');
 const fs = require('fs');
 
-global.KEYS = require('../../keys.js.config');
+global.KEYS = require('../../keys.js.default');
 global.CONFIG = require('../../config.js');
 const WhenIWork = CONFIG.WhenIWork;
 const dateFormat = 'YYYY-MM-DD HH:mm:ss';
--- www/canvas/index.js
+++ www/canvas/index.js
@@ -5,54 +5,18 @@ const canvas = require(CONFIG.root_dir + '/jobs/scheduling/helpers/updateCanvas.
 const stathat = require(CONFIG.root_dir + '/lib/stathat');
 const router = express.Router();
 
+const helpers = require(`${CONFIG.root_dir}/www/canvas/helpers.js`);
 const mandrill = require('mandrill-api/mandrill');
 const mandrillClient = new mandrill.Mandrill(KEYS.mandrill.api_key);
 
 router.put('/firstShift', function (req, res) {
   const assignment = CONFIG.canvas.assignments.attendedFirstShift;
   const user = req.body.user;
-  let promiseUserID;
-  let promiseCourseID;
-  let promiseAssignmentID;
 
-  // request canvas user with this email address and find their userId
-  promiseUserID = canvas.scrapeCanvasUsers(user.email)
-  .then((users) => {
-    if (users.length === 0) throw 'No user was found in Canvas for that email.';
-    return users[0].id;
-  }).catch(err => {
-    const subject = `Error finding user with email ${user.email} in Canvas`;
-    logErrEmailHeather(subject, err, user, assignment);
-  });
+  const logErrEmailHeather = createLogErrEmailHeatherFunc(`platform user:` +
+        `${JSON.stringify(user)} was not able to be given credit for ${assignment}`);
 
-  // request courses and find correct courseId;
-  promiseCourseID = promiseUserID.then((userID) => {
-    return canvas.scrapeCanvasEnrollment(userID);
-    })
-    .then((courses) => {
-    courses = courses.filter((course) => {
-      return course.enrollment_state === 'active';
-    });
-    if (courses.length === 0) throw 'No active courses for that user were found in Canvas.';
-    return courses[0].course_id;
-  }).catch(err => 
-    CONSOLE_WITH_TIME(`Error finding active course for ${user.email}: ${err}`)
-  );
-
-  // request assignments for course and find correct assignment
-  promiseAssignmentID = promiseCourseID.then((courseID) => {
-    return canvas.scrapeCanvasAssignments(courseID, assignment);
-  })
-  .then(assignments => {
-    if (assignments.length === 0) throw `Canvas returned 0 assignments`;
-    return assignments[0].id;
-  }).catch(err => {
-    const subject = `Error finding ${assignment} in course found for ${user.email}`;
-    logErrEmailHeather(subject, err, user, assignment);
-  });
-
-
-  Promise.all([promiseUserID, promiseCourseID, promiseAssignmentID])
+  canvas.retrieveUserCourseAssignmentIds(user.email, assignment, logErrEmailHeather)
   .then(responses => {
     // responses is [userID, courseID, assignmentID] so we spread it
     return canvas.updateUserGrade(...responses, 'complete');
@@ -68,26 +32,53 @@ router.put('/firstShift', function (req, res) {
   stathat.increment('Canvas - Attend First Shift', 1);
 });
 
-function logErrEmailHeather (subject, err, user, assignment) {
-  CONSOLE_WITH_TIME(`${subject}: ${err}`);
+router.post('/typeformAssignment/:assignment', function (req, res) {
+  const formResponse = req.body.form_response;
+  const emailWithSubmission = helpers.extractEmailAndFormatSubmission(formResponse);
+  const assignment = req.params.assignment;
+  const logErrEmailHeather = createLogErrEmailHeatherFunc(`typeform submission for email:` +
+        `${emailWithSubmission.email} was not able to be given credit for ${assignment}`);
 
-  const message = {
-    subject: subject,
-    text: `Hi Heather, Due to the specified error in the subject platform user:` +
-      `${JSON.stringify(user)} was not able to be given credit for ${assignment}`,
-    from_email: 'support@crisistextline.org',
-    from_name: 'Crisis Text Line',
-    to: [{
-        email: 'heather@crisistextline.org',
-        name: 'Heather',
-        type: 'to'
-    }]
-  };
-
-  mandrillClient.messages.send({message: message, key: 'canvas_user_not_found'}, function (res) {
-      CONSOLE_WITH_TIME(res);
+  canvas.retrieveUserCourseAssignmentIds(emailWithSubmission.email, assignment, logErrEmailHeather)
+  .then(responses => {
+    // responses is [userID, courseID, assignmentID] so we spread it
+    const promises = [canvas.submitAssignment(...responses, emailWithSubmission.submissionHTML)];
+    if (formResponse.calculated && formResponse.calculated.score) {
+      promises.push(canvas.updateUserGrade(...responses, formResponse.calculated.score));
+    }
+    return Promise.all(promises);
+  }).then(() => res.send({message: `Successfully submitted ${assignment} with ${JSON.stringify(emailWithSubmission)}`}))
+  .catch((err) => {
+    const message = `Submitting ${assignment} for ${emailWithSubmission.email} in Canvas failed: ${err}`;
+    CONSOLE_WITH_TIME(message);
+    res.status(500).send({message});
   });
+});
 
+
+function createLogErrEmailHeatherFunc (messageText) {
+
+  return (subject, err) => {
+    CONSOLE_WITH_TIME(`${subject}: ${err}`);
+
+    const message = {
+      subject: subject,
+      text: `Hi Heather, Due to the error specified in the subject ` + messageText,
+      from_email: 'support@crisistextline.org',
+      from_name: 'Crisis Text Line',
+      to: [{
+          email: 'heather@crisistextline.org',
+          name: 'Heather',
+          type: 'to'
+      }]
+    };
+
+    mandrillClient.messages.send({message: message, key: 'canvas_user_not_found'}, function (res) {
+        CONSOLE_WITH_TIME(res);
+    });
+  };
+  
 }
+
 
 module.exports = {router};
