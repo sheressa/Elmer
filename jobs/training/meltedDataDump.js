ebd78e8edfcae8455a8061129b2959ab08a36c35
Michael Howard <TurboNerd@Michaels-MacBook-Air.local>
2016-09-19 16:23:47 -0400

    added method to get user emails

--- jobs/training/meltedDataDump.js
+++ jobs/training/meltedDataDump.js
@@ -7,37 +7,52 @@ const KEYS = require('./keys.js');
 // new CronJob(CONFIG.time_interval.graduate_users_cron_job_string, function () {
 //     pollCanvasForGraduatedUsersThenCreatePlatformAccount();
 //   }, null, true);
-
+function masterConsoleTest(res){
+	console.log(res);
+	return res;
+}
 function go(){
 	getStudentsFromCohort()
 	.then(getEnrollmentsFromCohort)
-	.catch(function(res){
-		console.log('error', res)
+	.then(getUserEmailsForEachCohort)
+	.then(masterConsoleTest)
+	.catch(function(err){
+		console.error('Error: ', err)
 	})
 }
 
-
 function getStudentsFromCohort(){
 	return new Promise(function(resolve, reject){
-		request(`accounts/${KEYS.canvas.accountID}/courses`, 'GET')
+		request(`accounts/${KEYS.canvas.accountID}/courses?per_page=1000&enrollment_type=student`, 'GET')
 		  	.then(function (courses) {
 	    		var previous;
 		    	var cohorts = {};
 		    	courses.forEach(function (course) {
 					if (course.workflow_state!='available') return; 
-					var cohortNumber = course.name.trim().split(' ')[1];
+					//**parses cohortNumber from 'course name'. regex may be better
+					//if cohort naming changes format, the entire Cronjob will break
+					//maybe look for better methods for retrieving cohort number
+					var nameArr = course.name.trim().split(' ');
+					var cohortNumber = nameArr[1];
 					if(isNaN(cohortNumber)) return;
 					if(!cohorts[cohortNumber]){
-						cohorts[cohortNumber] = [];
+						cohorts[cohortNumber] = {};
+						cohorts[cohortNumber].class_ids = [];
+						cohorts[cohortNumber].trainers = [];
 					}
-					if(previous && previous === cohortNumber){
-						cohorts[cohortNumber].push(course.id);
-					}
-					previous = cohortNumber;
+					cohorts[cohortNumber].class_ids.push(course.id);
+					//***pulls trainer name from course name. Should it pull trainer user id? 
+					//temporary, each cohort has multiple trainers for each class. Need to change this
+					var trainer = nameArr[nameArr.length -1]
+					if(cohorts[cohortNumber].trainers.indexOf(trainer) < 0){
+						cohorts[cohortNumber].trainers.push(trainer);
+		    		}
 				});
 				resolve(cohorts);
-			});
-	});
+			})
+			.catch(errorHandler);
+	})
+		.catch(errorHandler);
 }
 
 function getEnrollmentsFromCohort(cohorts){
@@ -48,42 +63,113 @@ function getEnrollmentsFromCohort(cohorts){
 			prev[curr]['enrollment'] = null;
 			return prev;
 		}, {});
-//Not working right because of Promise chain stuff. Rewrite 'enrollmentLengthAPICall' 
-// and for loop using: http://stackoverflow.com/questions/24660096/correct-way-to-write-loops-for-promise
-
-		//API call to get enrollment for each course (info for users)
 		function enrollmentLengthAPICall(courseNum){
-			return request(`courses/${courseNum}/enrollments`)
+			return request(`courses/${courseNum}/enrollments?per_page=1000&enrollment_type=student`, 'GET')
 					.then(function(enrollment){
-						console.log('Enrollment length: ', enrollment.length);
 						return enrollment.length;
-					});
+					})
+					.catch(errorHandler);
 		}
-
-		//for each cohort, call API for enrollment and tally in 'courses' object
+		//for each cohort, call API for enrollment and tally in 'cohortEnrollments' object
+		var sumTallyPromiseArray = [];	
 		for (var key in cohorts){
 			if(!cohorts.hasOwnProperty(key)){
 				continue;
 			}
-			var students = 0;
-			for(var courseNumIter = 0; courseNumIter < cohorts[key].length; courseNumIter++){
-				students += enrollmentLengthAPICall(cohorts[key][courseNumIter]);
-
-			}
-			cohortEnrollments[key].enrollment = students;
+			var classes = cohorts[key].class_ids;
+			var classAPICalls = Promise.all(classes.map(enrollmentLengthAPICall));
+			var sumTallyPromise = classAPICalls
+				.then(function(data){
+					return data.reduce(function(a,b){ return a+b;}, 0);
+				})
+				.catch(errorHandler);
+			cohortEnrollments[key] = sumTallyPromise;
+			sumTallyPromiseArray.push(sumTallyPromise);
 		}
-		console.log(cohortEnrollments);
-		resolve(cohortEnrollments);
+		//wait for class enrollment API calls and enrollment tallies, then put into output object
+		var promiseToWaitForEnrollmentLengthToTally = Promise.all(sumTallyPromiseArray);
+		promiseToWaitForEnrollmentLengthToTally
+			.then(function(res){
+				Object.keys(cohortEnrollments).forEach(function(key){
+					cohortEnrollments[key].then(function(val){
+						cohorts[key]['enrollment'] = val;
+					});
+				});
+				resolve(cohorts);
+			})
+			.catch(errorHandler);
 	});
 }
 
+function getUserEmailsForEachCohort(cohorts){
+	return new Promise(function(resolve, reject){
+		var cohortKeys = Object.keys(cohorts);
+		var cohortEmails = {};
+		var cohortPromises = {};
+		var allPromises =[];
+		cohortKeys.forEach(function(key){
+			var classes = cohorts[key].class_ids;
+			cohorts[key].emails = [];
+			cohortPromises[key] = classes.map(function(class_id){
+					return request(`courses/${class_id}/users?per_page=1000&enrollment_type=student`, 'GET')
+						.then(function(users){
+							return users.map(function(user){
+								return user.login_id;
+							});
+						});
+			});
+			allPromises.push(cohortPromises[key]);
+		});
+		allPromises = allPromises.reduce(function(prev,curr){
+			return prev.concat(curr);
+		});
+		var finalPromiseCheck = Promise.all(allPromises);
+		finalPromiseCheck
+			.then(function(res){
+				var promiseCheck = [];
+				cohortKeys.forEach(function(key){
+					cohortPromises[key].forEach(function(cohortP){
+						var emailResolvePromise = cohortP.then(function(userEmails){
+							cohorts[key].emails.push(userEmails);
+						});
+						promiseCheck.push(emailResolvePromise);
+					});
+				});
+
+				Promise.all(promiseCheck)
+					.then(function(res){
+						cohortKeys.forEach(function(key){
+							cohorts[key].emails = cohorts[key].emails.reduce(function(prev,curr){
+								return prev.concat(curr);
+							});
+						});
+						resolve(cohorts);
+					});
+			});
+	});
+}
+
+// function testFunction(){
+// 	return new Promise(function(resolve, reject){
+// 		request('courses/75/users?per_page=1000', 'GET')
+// 			.then(function(users){
+// 				console.log('Test Users: ', users.length);
+// 				users.forEach(function(user){
+// 					console.log(user.name);
+// 				});
+// 			});
+// 	});
+// }
+
+//wrap in Cronjob
 go();
-
-
-
-
+// testFunction();
 
 //HELPER FUNCTIONS
+function errorHandler(err){
+	console.error('Error: ', err);
+}
+
 function convertIds(text) {
   var regex = /":(\d+),/g;
   var id;
@@ -102,7 +188,7 @@ function request(url, method, params) {
   if (!params) {
     params = [];
   }
-  params.push('per_page=1000');
+  //params.push('per_page=1000');
 
   url = 'https://crisistextline.instructure.com/api/v1/' + url;
 
